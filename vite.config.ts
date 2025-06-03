d3daf749f3ff78f2e1c1f1d6a2757c9f4fdaf9f4
harshendra-upadhyay-11 <harshendra.upadhyay@neosoftmail.com>
2025-06-03 19:02:37 +0530

    commit for deeployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,37 +3,46 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@guards": path.resolve(__dirname, "./src/components/guards"),
-      "@layouts": path.resolve(__dirname, "./src/layouts"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@features": path.resolve(__dirname, "./src/features"),
-      "@services": path.resolve(__dirname, "./src/services"),
-      "@api": path.resolve(__dirname, "./src/api"),
-      "@redux": path.resolve(__dirname, "./src/redux"),
-      "@store": path.resolve(__dirname, "./src/redux/store"),
-      "@hooks": path.resolve(__dirname, "./src/redux/hooks"),
-      "@slices": path.resolve(__dirname, "./src/redux/slices"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
-      "@constants": path.resolve(__dirname, "./src/constants"),
-      "@types": path.resolve(__dirname, "./src/types"),
+export default defineConfig(({ command }) => {
+  const isDev = command === "serve";
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@guards": path.resolve(__dirname, "./src/components/guards"),
+        "@layouts": path.resolve(__dirname, "./src/layouts"),
+        "@pages": path.resolve(__dirname, "./src/pages"),
+        "@features": path.resolve(__dirname, "./src/features"),
+        "@services": path.resolve(__dirname, "./src/services"),
+        "@api": path.resolve(__dirname, "./src/api"),
+        "@redux": path.resolve(__dirname, "./src/redux"),
+        "@store": path.resolve(__dirname, "./src/redux/store"),
+        "@hooks": path.resolve(__dirname, "./src/redux/hooks"),
+        "@slices": path.resolve(__dirname, "./src/redux/slices"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
+        "@styles": path.resolve(__dirname, "./src/styles"),
+        "@constants": path.resolve(__dirname, "./src/constants"),
+        "@types": path.resolve(__dirname, "./src/types")
+      }
     },
-  },
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+    build: {
+      outDir: "dist"
     },
-  },
+    server: isDev
+      ? {
+          port: 3000,
+          open: true,
+          proxy: {
+            "/api": {
+              target: "http://localhost:3001",
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/api/, "")
+            }
+          }
+        }
+      : undefined
+  };
 });
